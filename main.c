7e17c0cc538398d999778d573e6c7a55cfc58df2
Rabonaa <rabemanantsoa.ma@gmail.com>
2025-11-07 00:58:57 +0100

    Update

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include "utils.h"
 
 int main() {
-    t_liste_adj liste_adj = readGraph("exemple1.txt");
+    t_liste_adj liste_adj = readGraph("exemple_valid_step3.txt");
     DisplayAdjList(liste_adj, liste_adj.taille);
     isMarkovGraph(liste_adj);
-    WriteGraph("Test_dessin", liste_adj);
+    WriteGraph("graph.txt", liste_adj);
     return 0;
 }
